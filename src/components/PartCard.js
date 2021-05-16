a18e610a28f295b3f6e0baba4fad46029e3443d5
Luke Savage <luke.savage92@gmail.com>
2021-05-16 17:42:20 -0500

    proj init and create part card

---
+++ src/components/PartCard.js
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+const PartCard = () => {
+  let [quantity, setQuantity] = useState(null);
+  return (
+    <div>
+      <span>PART_NAME_HERE</span>
+      <div className="card-actions">
+        <input
+          type="text"
+          placeholder="Quantity"
+          value={quantity}
+          onChange={(e) => setQuantity(e.target.value)}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default PartCard;
